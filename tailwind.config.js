30d21622216edef2cd9c76a34358be1555843912
KuangPF <1633397286@qq.com>
2021-10-10 21:19:46 +0800

    chore(config): add keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,15 @@ module.exports = {
       },
       boxShadow: {
         DEFAULT: '0 1px 2px rgb(0 0 0 / 10%)'
+      },
+      keyframes: {
+        ring: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' }
+        }
+      },
+      animation: {
+        ring: 'ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite'
       }
     }
   },
