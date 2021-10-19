3935fc53274afb054562da538744a5464c575337
KuangPF <1633397286@qq.com>
2021-10-19 23:15:00 +0800

    feat(user): user page dev

--- pages/user/[id].jsx
+++ pages/user/[id].jsx
@@ -8,7 +8,19 @@ const User = () => {
   return (
     <div>
       <CommonHeader />
-      <div>{id}</div>
+      <div className="max-w-screen-md mt-0 mb-0 mr-auto ml-auto px-7 py-8 bg-white text-gray-600 text-s">
+        <h1 className="text-2xl font-bold">{`User : ${id}`}</h1>
+        <ul className="my-4">
+          <li>
+            <span className="mr-1">Created: </span>
+            3378 days ago
+          </li>
+          <li>
+            <span className="mr-1">Karma: </span>
+            898
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
